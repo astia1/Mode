4debca32e89c3a7f5d12702f44b8e4e2b7567946
John McKinney <john@mckinneyphoto.com>
2021-09-15 17:28:29 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function mode(array) {
+  array.sort((function(a, b){return a - b}));
+  let modeArray = []
+  modeArray.push(array[0]);  
+  for ( let i = 0, x = 0 ; i < array.length ; i += 1 ) {
+  if (modeArray[0] == array[i]) { 
+  modeArray.splice(x,1,array[i]);
+  x++
+  }
+  else { 
+  modeArray.splice(0,1,array[i]);
+
+  modeArray [x = 1];
+
+     }
+
+  }
+  console.log(modeArray)
+  return [modeArray.pop()]
+}
+
+// Extension: solve this in 0(n) time
+
+function modeTests() {
+  console.log(mode([1,2,2,1,1,3, 7, 3]), ' -> 1');
+  console.log(mode([1]), '1');
+  console.log(mode([2, 2, 2, 3, 3, 3,3,9,9,9,4,4,4]), ' -> 3');
+}
+
+modeTests() // uncomment to test!
